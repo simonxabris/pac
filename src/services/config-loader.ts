ef03c8e2f3a64431ec11cf7a960264fac766916f
Abris Simon <abris96@gmail.com>
2026-06-12 14:28:39 +0200

    initial event api version

--- src/services/config-loader.ts
+++ src/services/config-loader.ts
@@ -3,7 +3,8 @@ import { pathToFileURL } from "node:url";
 import { Effect, Layer, Schema } from "effect";
 import * as Context from "effect/Context";
 import type { DesiredResource } from "../core/resource.js";
-import { getResources, resetRegistry } from "../resources/registry.js";
+import type { EventDefinition } from "../events/event.js";
+import { getEventDefinitions, getResources, resetRegistry } from "../resources/registry.js";
 import { errorMessage } from "../utils.js";
 
 export class UserConfigLoadError extends Schema.TaggedErrorClass<UserConfigLoadError>()(
@@ -14,15 +15,23 @@ export class UserConfigLoadError extends Schema.TaggedErrorClass<UserConfigLoadE
   },
 ) {}
 
-const loadDesiredResources = (
+export type LoadedConfig = {
+  readonly desiredResources: ReadonlyArray<DesiredResource>;
+  readonly eventDefinitions: ReadonlyArray<EventDefinition>;
+};
+
+const loadConfig = (
   configPath = "pac.config.ts",
-): Effect.Effect<ReadonlyArray<DesiredResource>, UserConfigLoadError> =>
+): Effect.Effect<LoadedConfig, UserConfigLoadError> =>
   Effect.tryPromise({
     try: async () => {
       resetRegistry();
       const absolutePath = resolve(process.cwd(), configPath);
       await import(`${pathToFileURL(absolutePath).href}?t=${Date.now()}`);
-      return getResources().map((resource) => resource.toDesiredResource());
+      return {
+        desiredResources: getResources().map((resource) => resource.toDesiredResource()),
+        eventDefinitions: [...getEventDefinitions()],
+      };
     },
     catch: (cause) =>
       new UserConfigLoadError({
@@ -34,15 +43,13 @@ const loadDesiredResources = (
 export class ConfigLoader extends Context.Service<
   ConfigLoader,
   {
-    readonly loadDesiredResources: (
-      configPath?: string,
-    ) => Effect.Effect<ReadonlyArray<DesiredResource>, UserConfigLoadError>;
+    readonly loadConfig: (configPath?: string) => Effect.Effect<LoadedConfig, UserConfigLoadError>;
   }
 >()("@app/ConfigLoader") {
   static readonly layer = Layer.succeed(
     ConfigLoader,
     ConfigLoader.of({
-      loadDesiredResources,
+      loadConfig,
     }),
   );
 }
